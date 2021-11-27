fdac988f4c9e574682eb0de1aeed6d0d3c183df7
Bryan Arendt <bryanarendt@gmail.com>
2021-11-27 08:33:19 -0500

    finish tailwind config

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import type { LinksFunction } from "remix";
 import {
   Link,
   Links,
@@ -8,21 +9,12 @@ import {
   ScrollRestoration,
   useCatch
 } from "remix";
-import type { LinksFunction } from "remix";
+import tailwindUrl from "~/styles/app.css";
 
-import globalStylesUrl from "~/styles/global.css";
-import darkStylesUrl from "~/styles/dark.css";
 
 // https://remix.run/api/app#links
 export let links: LinksFunction = () => {
-  return [
-    { rel: "stylesheet", href: globalStylesUrl },
-    {
-      rel: "stylesheet",
-      href: darkStylesUrl,
-      media: "(prefers-color-scheme: dark)"
-    }
-  ];
+  return [{ rel: "stylesheet", href: tailwindUrl }];
 };
 
 // https://remix.run/api/conventions#default-export
---
+++ setup.md
@@ -0,0 +1,4 @@
+# install tailwind
+```
+npm install tailwindcss @tailwindcss/aspect-ratio @tailwindcss/forms @heroicons/react concurrently
+```
