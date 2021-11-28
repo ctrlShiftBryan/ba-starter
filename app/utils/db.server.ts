8d06e93fcea3cd029e08de3be62da1ec8cd5e976
Bryan Arendt <bryanarendt@gmail.com>
2021-11-27 23:43:10 -0500

    add prisma

---
+++ app/utils/db.server.ts
@@ -0,0 +1,28 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable import/no-mutable-exports */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-var */
+import { PrismaClient } from '@prisma/client';
+
+var db: PrismaClient;
+
+declare global {
+  var __db: PrismaClient | undefined;
+}
+
+// this is needed because in development we don't want to restart
+// the server with every change, but we want to make sure we don't
+// create a new connection to the DB with every change either.
+if (process.env.NODE_ENV === 'production') {
+  db = new PrismaClient();
+  db.$connect();
+} else {
+  if (!global.__db) {
+    global.__db = new PrismaClient();
+    global.__db.$connect();
+  }
+  db = global.__db;
+}
+
+export { db };
