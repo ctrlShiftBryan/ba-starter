44915ba81c272ddb4fa71d25b103685e33e65b7c
Bryan Arendt <bryanarendt@gmail.com>
2021-11-28 16:47:27 -0500

    catch boundary

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,20 +1,24 @@
 import { Joke } from '@prisma/client';
-import { LoaderFunction, useLoaderData } from 'remix';
+import {
+  LoaderFunction, useCatch, useLoaderData, useParams
+} from 'remix';
 import { db } from '~/utils/db.server';
 
 type LoaderData = { joke: Joke | null}
 
 export const loader: LoaderFunction = async ({ params }) => {
   // throw new Error('whoops');
-  const data: LoaderData = {
-    joke:
-   await db.joke.findUnique({
-     where: {
-       id: params.jokeId
-     }
-   })
-  };
-  return data;
+  const joke = await db.joke.findUnique({
+    where: {
+      id: params.jokeId
+    }
+  });
+  if (!joke) {
+    throw new Response('What a joke! Not found.', {
+      status: 404
+    });
+  }
+  return { joke };
 };
 
 export default function JokeRoute() {
@@ -30,7 +34,20 @@ export default function JokeRoute() {
     </div>
   );
 }
-
+export function CatchBoundary() {
+  const caught = useCatch();
+  const params = useParams();
+  if (caught.status === 404) {
+    return (
+      <div className="p-3 m-2 text-red-500 bg-red-300">
+        Huh? What the heck is "
+        {params.jokeId}
+        "?
+      </div>
+    );
+  }
+  throw new Error(`Unhandled error: ${caught.status}`);
+}
 export const ErrorBoundary = function ({ error }: { error: Error }) {
   // eslint-disable-next-line no-console
   console.error(error);
